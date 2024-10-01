c9598918fcb184db7892ea4a97c9b3aa4c606e16
Sasidurka Venkatesan <venkasas@sheridancollege.ca>
2024-10-01 07:54:23 -0400

    Initial Commit

---
+++ MyPizzaOrderSasidurka/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPizzaOrderSasidurka
+//
+//  Created by Sasidurka on 2024-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPizzaOrderSasidurka/MyPizzaOrderSasidurkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPizzaOrderSasidurkaApp.swift
+//  MyPizzaOrderSasidurka
+//
+//  Created by Sasidurka on 2024-10-01.
+//
+
+import SwiftUI
+
+@main
+struct MyPizzaOrderSasidurkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
